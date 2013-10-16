d1a71c906c74cf2df43e31d62af9aeb15aa03672
Andrey Antukh <niwi@niwi.be>
2013-10-16 10:16:35 +0200

    Minor change on groovy bench file.

--- groovy_bench/src/main/groovy/test/App.groovy
+++ groovy_bench/src/main/groovy/test/App.groovy
@@ -3,7 +3,7 @@ package test
 import java.util.Random
 
 public class App {
-    static ArrayList<Integer> generateRandomList(num) {
+    ArrayList<Integer> generateRandomList(num) {
         Random rand = new Random()
         ArrayList<Integer> list = new ArrayList<Integer>()
 
@@ -14,29 +14,27 @@ public class App {
         return list
     }
 
-    static ArrayList<ArrayList<Integer>> getTestList() {
+    ArrayList<ArrayList<Integer>> getTestList() {
         ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>()
 
         (1..100).each {
-            list << App.generateRandomList(500)
+            list << generateRandomList(500)
         }
 
         return list
     }
 
-    static void benchFn1(data) {
-        def bench = {
-            def sum1 = data.collect { it.inject(0) { acc, val -> acc + val } }
-            return sum1.inject(0) { acc, val -> acc + val }
-        }
-
-        def start = System.nanoTime()
-        def result = bench()
-        println("[Groovy 01]  Elapsed time: " + ((System.nanoTime() - start) / 1000000.0 ) + " msecs ( Result: " + result + " )")
+    int benchFn1(data) {
+        ArrayList<Integer> sum1 = data.collect { it.inject(0) { acc, val -> acc + val } }
+        return sum1.inject(0) { acc, val -> acc + val }
     }
 
     public static void main(String[] args) {
-        def testList = App.getTestList()
-        benchFn1(testList)
+        App appInstance = new App()
+        ArrayList<ArrayList<Integer>> testList = appInstance.getTestList()
+
+        long start = System.nanoTime()
+        int result = appInstance.benchFn1(testList)
+        println("[Groovy 01]  Elapsed time: " + ((System.nanoTime() - start) / 1000000.0 ) + " msecs ( Result: " + result + " )")
     }
 }
