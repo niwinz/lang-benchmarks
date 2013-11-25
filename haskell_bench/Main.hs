d1b1902bee88ffd6a6511ffd308e4bc64e27a926
Alejandro Gómez <alejandro@dialelo.com>
2013-11-25 19:06:50 +0100

    Update haskell benchmark to accept arguments

--- haskell_bench/Main.hs
+++ haskell_bench/Main.hs
@@ -2,22 +2,43 @@
 module Main where
 
 import Control.Monad (replicateM)
+import Network.CGI.Protocol (maybeRead)
+import System.Environment (getArgs)
 import System.Random (newStdGen, randomRs)
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 import Text.Printf (printf)
 
-sumLists :: [[Integer]] -> Integer
+defaultLists = 100
+defaultElements = 500
+
+sumLists :: [[Int]] -> Int
 sumLists = sum . concat
 
-randomList :: IO [Integer]
-randomList = do g <- newStdGen
-                let !rs = take 500 $ randomRs (0, 100) g
-                return rs
+randomList n = do g <- newStdGen
+                  let !rs = take n $ randomRs (0, 100) g
+                  return rs
 
 showDiffTimeInMs dt = timeInMs ++ " msecs"
          where timeInMs = init . show $ 1000 * dt
 
-main = do randomLists <- replicateM 100 randomList
+readInt :: String -> Int
+readInt = read
+
+readIntOr :: Int -> String -> Int
+readIntOr d s = case parsedString of
+                   Nothing -> d
+                   Just x -> x
+                where parsedString = maybeRead s :: Maybe Int
+
+parseArgs :: [String] -> (Int, Int)
+parseArgs [] = (defaultLists, defaultElements)
+parseArgs (x:[]) = (readIntOr defaultLists x, defaultElements)
+parseArgs (x:y:_) = (readIntOr defaultLists x, readIntOr defaultElements y)
+
+main = do args <- getArgs
+          let (listAmount, elementAmount) =  parseArgs args
+
+          randomLists <- replicateM listAmount $ randomList elementAmount
 
           before <- getCurrentTime
           let !result = sumLists randomLists
